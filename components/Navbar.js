02d0a4af5dd4644729e9dd24d43e13ea6e3364ad
arjun <arjun232001@gmail.om>
2022-08-25 03:37:40 +0530

    init

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+//if !user
+//search -> logo -> login
+//else
+//logo -> search -> username -> cart
+export default function Navbar(){
+    return(
+        <>
+
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
 
   return (
-    <div className={styles.container}>
-      Hey I am store aap
+    <div className=''>
     </div>
   )
 }
