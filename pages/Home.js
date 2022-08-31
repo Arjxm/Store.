d590da5b95a832ca5f88a6af3a61f169c5f62065
arjunchauhan <arjun232001@gmail.com>
2022-08-31 17:57:14 +0530

    UI

---
+++ pages/Home.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar/Navbar";
+import {Footer} from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+
+export default function Main(){
+    return(
+        <>
+            <Navbar/>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Navbar from "../components/Navbar/Navbar";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Main from "./Home";
+
 export default function Home() {
 
   return (
-    <div className=''>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path = '/' element={<Main/>}/>
+
+        </Routes>
+    </BrowserRouter>
   )
 }
