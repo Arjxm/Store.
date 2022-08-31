59414f3028d67e9f64390abb0194807b652e0e5d
arjunchauhan <arjun232001@gmail.com>
2022-08-31 18:17:52 +0530

    Fix

--- pages/Cart.js
+++ pages/Cart.js
@@ -2,7 +2,7 @@ export default  function Cart(){
     return(<>
     <h1>CART</h1>
 
-
+<h1>Hello World</h1>
 
 
     </>)
--- pages/Home.js
+++ pages/Home.js
@@ -7,6 +7,9 @@ export default function Main(){
         <>
             <Navbar/>
             <Header/>
+
+
+
             <Footer/>
         </>
     )
