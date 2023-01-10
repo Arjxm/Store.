dcc8ede8e0c7e71ea38654f30cb2ce108f2a9fd1
arjunchauhan <arjun232001@gmail.com>
2023-01-11 01:00:56 +0530

    init

---
+++ ui/src/Components/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar/NavBar";
+import Footer from "./Footer/Footer";
+
+function App() {
+  return (
+    <>
+        <NavBar/>
+        <Footer/>
+    </>
+  );
+}
+
+export default App;
